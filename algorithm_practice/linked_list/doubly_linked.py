ea22853ffa411b2e9ac771a3c3d21686627a3da5
grace lee <glee8804@gmail.com>
2020-12-29 15:00:34 +0000

    doubly linked list

--- algorithm_practice/linked_list/doubly_linked.py
+++ algorithm_practice/linked_list/doubly_linked.py
@@ -6,28 +6,31 @@
 # delete O(n)
 
 class Node:
-  def __init__(self, value, next):
+  def __init__(self, value, prev, next):
     self.value = value
+    self.prev = prev
     self.next = next
 
-class LinkedList:
+class DoubleLinkedList:
   def __init__(self):
     self.head = None
     self.tail = None
     self.length = 0
 
   def append(self, value):
-    node = Node(value, None)
     if self.length ==0: #new
+      node = Node(value, None, None)
       self.head = node
       self.tail = node
     else:
+      node = Node(value, self.tail, None)
       self.tail.next = node
       self.tail = node
     self.length += 1
   
   def prepend(self, value):
-    node = Node(value, self.head)
+    node = Node(value, None, self.head)
+    self.head.prev = node
     self.head = node
     self.length += 1
   
@@ -43,41 +46,39 @@ class LinkedList:
     elif self.length < index:
       self.append(value)
     else:
-      cur_node = self.traverse_to(index)
-      node = Node(value, cur_node.next)
-      cur_node.next = node
+      prev_node = self.traverse_to(index)
+      next_node = prev_node.next
+      node = Node(value, prev_node, next_node)
+      prev_node.next = node
+      next_node.prev = node
       self.length +=1
   
   def remove(self, index):
     if self.length > index:
       prev_node = self.traverse_to(index-1)
       cur_node = prev_node.next
-      prev_node.next = cur_node.next
+      next_node = cur_node.next
+      prev_node.next = next_node
+      next_node.prev = prev_node
       del cur_node
       self.length -= 1
 
-  def reverse(self):
-    if self.length < 2:
-      return
-    self.tail = self.head
-    prev = None
-    cur_node = self.head
-    while self.head != None:
-      self.head = cur_node.next
-      cur_node.next = prev
-      prev = cur_node
+  def print(self, reverse=False):
+    if reverse:
+      cur_node = self.tail
+      while cur_node != None:
+        print(cur_node.value , end = ' <- ')
+        cur_node = cur_node.prev
+      print()
+    else:
       cur_node = self.head
-    self.head = prev
-  
-  def print(self):
-    cur_node = self.head
-    while cur_node != None:
-      print(cur_node.value , end = ' -> ')
-      cur_node = cur_node.next
-    print()
+      while cur_node != None:
+        print(cur_node.value , end = ' -> ')
+        cur_node = cur_node.next
+      print()
 
 
-my_linked_list = LinkedList()
+my_linked_list = DoubleLinkedList()
 my_linked_list.append(10)
 my_linked_list.append(5)
 my_linked_list.prepend(16)
@@ -86,10 +87,9 @@ my_linked_list.insert(0, 9)
 my_linked_list.insert(22, 69)
 print(my_linked_list.length) 
 my_linked_list.print() #9, 16, 10, 99, 5, 
+my_linked_list.print(True) 
 
 my_linked_list.remove(1)
 print(my_linked_list.length)
 my_linked_list.print() #9, 10, 99, 5, 
-
-my_linked_list.reverse()
-my_linked_list.print()
+my_linked_list.print(True) 
